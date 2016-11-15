967417fb3b879920093b376b3074c1fe91c7b4a0
Xeaphii <muhammadzeeshan020@gmail.com>
2016-11-15 18:52:06 +0500

    Adding privs

--- Namespaces/tablespacetesing.sql
+++ Namespaces/tablespacetesing.sql
@@ -1,9 +1,9 @@
-/* Formatted on 11/15/2016 5:42:38 PM (QP5 v5.227.12220.39724) */
+/* Formatted on 11/15/2016 6:47:29 PM (QP5 v5.227.12220.39724) */
 --Creating in the table space
 
-drop table whoopie;
+DROP TABLE whoopie;
 
-drop view whoopie;
+DROP VIEW whoopie;
 
 CREATE TABLE whoopie
 (
@@ -13,17 +13,39 @@ CREATE TABLE whoopie
 
 --trying to create view with same name
 --replace will replace table definition no matter  of other object
+
 CREATE OR REPLACE VIEW whoopie
 AS
    SELECT * FROM ports;
-   
-   
+
+
    --trying to create view with same name
 
 CREATE VIEW whoopie
 AS
    SELECT * FROM ports;
-   
-   
-select * from whoopie;
 
+-- selecting from table or view
+
+SELECT * FROM whoopie;
+
+CREATE INDEX whoopie
+   ON whoopie (foo);
+
+
+ALTER TABLE whoopie
+ADD CONSTRAINT whoopie UNIQUE (bar);
+
+
+SELECT *
+  FROM sys.dba_objects
+ WHERE object_name = 'WHOOPIE';
+
+SELECT *
+  FROM USER_TAB_PRIVS
+ WHERE table_name = 'DBA_OBJECTS' ;
+ 
+ SELECT * FROM dba_objects WHERE object_name LIKE 'V$%';
+ 
+ 
+ 
